0f87d067731c58607ac4bbbfc260b09a88da9bd6
Rositsa Ilieva-Kitova <105305366+RosiIIK@users.noreply.github.com>
2022-09-23 15:09:24 +0300

    Create 06_shopping.py

---
+++ basic_syntax_conditional_statements_and_loops/06_shopping.py
@@ -0,0 +1,14 @@
+budget = int(input())
+
+command = input()
+while command != "End":
+    price_per_product = int(command)
+    budget -= price_per_product
+    if budget < 0:
+        print("You went in overdraft!")
+        break
+          
+    command = input()
+          
+else:
+    print("You bought everything needed.")
